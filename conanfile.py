e980f6d0586bee6e952da272601530a86a3a8fbc
Luiz Gabriel <luizgabriel.info@gmail.com>
2020-04-09 18:23:57 -0300

    Updated conanfile

---
+++ conanfile.py
@@ -0,0 +1,23 @@
+from conans import ConanFile, tools
+
+class TOMLPPConan(ConanFile):
+    name = "toml++"
+    version = "1.2.0"
+    license = "MIT"
+    homepage = "https://github.com/marzer/tomlplusplus"
+    url = "https://github.com/luizgabriel/conan-tomlplusplus"
+    description = "TOML config parser and serializer for modern C++" \
+                  "macOS and WASM/WebGL "
+    topics = ("toml++", "toml", "parser")
+    exports_sources = "toml++/include/*"
+    no_copy_source = True
+
+    def source(self):
+        git = tools.Git(folder="toml++")
+        git.clone("https://github.com/marzer/tomlplusplus.git", "v" + self.version)
+
+    def package(self):
+        self.copy("*.h")
+
+    def package_id(self):
+        self.info.header_only()
