620bb73b0aa9da643c1f0cd772a6e2a4111ff25a
Luiz Gabriel <luizgabriel.info@gmail.com>
2020-05-30 11:46:21 -0300

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,6 @@ class TOMLPPConan(ConanFile):
     url = "https://github.com/luizgabriel/conan-tomlplusplus"
     description = "TOML config parser and serializer for modern C++"
     topics = ("toml++", "toml", "parser")
-    exports_sources = "toml++/include/*"
     no_copy_source = True
 
     def source(self):
@@ -16,7 +15,7 @@ class TOMLPPConan(ConanFile):
         git.clone("https://github.com/marzer/tomlplusplus.git", "v" + self.version)
 
     def package(self):
-        self.copy("*.h")
+        self.copy("*.h", dst="include", src="toml++/include/")
 
     def package_id(self):
         self.info.header_only()
